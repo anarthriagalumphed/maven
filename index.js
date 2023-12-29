67fb73229750bea6363b125d583e6938d57df05a
anarthriagalumphed <galih22.mp@gmail.com>
2023-12-29 16:17:44 +0700

    rollback

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const { Client, GatewayIntentBits, Collection, ActivityType } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
-
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// Konfigurasi dan login
-client.login(process.env.TOKEN)
-  .catch(error => {
-    console.error(`Error during login: ${error.message}`);
-  });
-
-client.slashCommands = new Collection();
-
-// Fungsi untuk mengubah status bot
-function updateStatus() {
-  const activities = [
-    { name: 'Your Bullshit', type: ActivityType.Listening, url: 'https://galihmahendrastudio.com/banner.png' },
-    { name: 'With Anarthria', type: ActivityType.Playing, url: 'https://galihmahendrastudio.com/banner.png' },
-    { name: 'Bunch of Codes', type: ActivityType.Watching, url: 'https://galihmahendrastudio.com/banner.png' },
-  ];
-
-  const randomActivity = activities[Math.floor(Math.random() * activities.length)];
-  client.user.setActivity(randomActivity.name, { type: randomActivity.type });
-}
-
 client.once('ready', async () => {
   console.log(`Your bot is Launch ${client.user.tag}`);
   updateStatus();
@@ -32,77 +5,42 @@ client.once('ready', async () => {
     updateStatus();
   }, 3600000);
 
-  try {
-    // Hapus cache perintah sebelumnya
-    await client.application.commands.fetch(); // Memastikan cache terisi
-    const existingCommands = client.application.commands.cache.array();
-    await Promise.all(existingCommands.map(cmd => cmd.delete()));
+  // Pendaftaran perintah global
+  const slashCommands = [
+    { name: 'ping', description: 'Ping command' },
+  ];
 
-    // Pendaftaran perintah global
-    const slashCommands = [
-      { name: 'ping', description: 'Ping command' },
-    ];
+  // Filter perintah yang sudah terdaftar
+  const existingCommands = await client.application.commands.fetch();
+  const newCommands = slashCommands.filter(cmd => !existingCommands.has(cmd.name));
 
-    // Register perintah-perintah baru
-    await client.application.commands.set(slashCommands);
+  // Register hanya perintah yang belum terdaftar
+  client.application.commands.set(newCommands);
 
-    // Pendaftaran perintah lokal (dalam folder 'commands')
-    const foldersPath = path.join(__dirname, 'commands');
-    const commandFolders = fs.readdirSync(foldersPath);
+  // Pendaftaran perintah lokal (dalam folder 'commands')
+  const foldersPath = path.join(__dirname, 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
 
-    for (const folder of commandFolders) {
-      const commandsPath = path.join(foldersPath, folder);
-      const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-      for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = require(filePath);
 
-        // Log file path for debugging
-        console.log(`Command file path: ${filePath}`);
+      // Log file path for debugging
+      console.log(`Command file path: ${filePath}`);
 
-        if ('data' in command && 'execute' in command) {
-          await client.application.commands.create(command.data);
-          client.slashCommands.set(command.data.name, command);
-        } else {
-          console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
+      if ('data' in command && 'execute' in command) {
+        await client.application.commands.create(command.data);
+        client.slashCommands.set(command.data.name, command);
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
       }
     }
-
-    console.log('Commands registered successfully.');
-  } catch (error) {
-    console.error(`Error during command registration: ${error.message}`);
   }
 
   // ... (rest of the code)
 
 });
-
-
-
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isCommand()) {
-    return;
-  }
-
-  const command = client.slashCommands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-    } else {
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-  }
-});
-
-// (Kode lainnya bisa ditambahkan di sini)
